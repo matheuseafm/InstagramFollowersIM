977ef5a4df203b3f5b401c24ad6c6ead4c4a9c1d
Matheus Moreira <matheus.moreira@investmoneysa.com.br>
2025-02-11 17:25:01 -0300

    Final Build

--- main.py
+++ main.py
@@ -2,27 +2,22 @@ import time
 import os
 from instagrapi import Client
 
-USERNAME = "iknowwhatudid_123"
-PASSWORD = "mem040503"
+USERNAME = ""
+PASSWORD = ""
 TARGET_USER = "instagram"
 
 def login():
     cl = Client()
     
-    # Tenta carregar a sessão, se existir
     if os.path.exists("session.json"):
         try:
             cl.load_settings("session.json")
         except Exception as e:
             print("Erro ao carregar a sessão:", e)
-    
-    # Tenta realizar o login com um código 2FA fixo (se você tiver um código válido no momento)
     try:
-        # Substitua "<2FA CODE HERE>" por um código válido ou deixe-o vazio para forçar a exceção
         cl.login(USERNAME, PASSWORD, verification_code="<2FA CODE HERE>")
     except Exception as e:
         print("Erro durante o login:", e)
-        # Se o código não for aceito, solicita manualmente o código 2FA atualizado:
         new_code = input("Digite o código 2FA atual: ").strip()
         try:
             cl.login(USERNAME, PASSWORD, verification_code=new_code)
@@ -48,7 +43,7 @@ if __name__ == "__main__":
             try:
                 cl.user_follow(user_id)
                 print(f"Seguiu: {user_id}")
-                time.sleep(50)  # Ajuste o tempo conforme necessário
+                time.sleep(50)
             except Exception as e:
                 print(f"Erro ao seguir {user_id}: {e}")
         print("Aguardando 15 minutos...")
